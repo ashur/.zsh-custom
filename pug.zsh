6d7a9c1561f7670cb4abb458e69c828577a66453
Ashur Cabrera <hello@cabreramade.co>
2017-06-08 08:13:49 -0700

    relocated pug_update definition

--- pug.zsh
+++ pug.zsh
@@ -1,6 +1,12 @@
 CURRENT_DATE=`date +%Y-%m-%d`
 PUG_UPDATE_FILE="${HOME}/.pug_update"
 
+function pug_update()
+{
+	pug update daily --all
+	echo $CURRENT_DATE > $PUG_UPDATE_FILE
+}
+
 if [ ! -f "${PUG_UPDATE_FILE}" ]; then
 	pug_update;
 fi
@@ -9,9 +15,3 @@ PUG_LAST_UPDATE=`cat ${PUG_UPDATE_FILE}`
 if [ "${CURRENT_DATE}" != "${PUG_LAST_UPDATE}" ]; then
 	pug_update;
 fi
-
-function pug_update()
-{
-	pug update daily --all
-	echo $CURRENT_DATE > $PUG_UPDATE_FILE
-}
