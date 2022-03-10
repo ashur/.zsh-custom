acb43c334dcacd8c550178d7ed270196e7390892
Ashur Cabrera <hello@ashur.cab>
2022-03-09 22:24:46 -0700

    aliases: Add cat → bat

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,5 @@
 alias bonjour="dns-sd -B _ssh._tcp ."
+alias cat=bat
 alias la="ls -hAlp"
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='src'
