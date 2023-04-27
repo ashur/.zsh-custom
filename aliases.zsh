3a86bb115f57175f96cd87e82c72b07f033cddf0
Ashur Cabrera <hello@ashur.cab>
2023-04-27 16:01:07 +0000

    aliases: Add ‘cap’
    Also removes ‘catpak’

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,7 @@
 alias bonjour="dns-sd -B _ssh._tcp ."
+alias cap="catpack"
 alias cat=bat
 alias catpack="cat package.json"
-alias catpak="cat package.json"
 alias la="ls -hAlp"
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='omz reload'
