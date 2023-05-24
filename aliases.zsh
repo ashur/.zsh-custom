62e672ff5178e447b42f37ec98108a670c87d757
Ashur Cabrera <hello@ashur.cab>
2023-05-24 16:12:21 -0600

    aliases: Add nova. typo catcher

--- aliases.zsh
+++ aliases.zsh
@@ -3,5 +3,6 @@ alias cap="catpack"
 alias cat=bat
 alias catpack="cat package.json"
 alias la="ls -hAlp"
+alias nova.="nova ."
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='omz reload'
