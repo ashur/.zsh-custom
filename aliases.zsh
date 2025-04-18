f99ed34b0fc940f0cdb245d02ad22b2af60b5301
Ashur Cabrera <hello@ashur.cab>
2025-04-18 10:19:11 -0700

    aliases: add serve function

--- aliases.zsh
+++ aliases.zsh
@@ -6,3 +6,16 @@ alias la="ls -hAlp"
 alias nova.="nova ."
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='omz reload'
+
+serve() {
+	if [ -f .aliases.zsh ]; then
+		source .aliases.zsh
+	fi
+
+	which serve_local > /dev/null
+	if [ $? -eq 0 ]; then
+		serve_local
+	else
+		npm run serve
+	fi
+}
