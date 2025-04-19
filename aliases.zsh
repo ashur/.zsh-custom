f5e7aa4ff384342bed47237e2778427079cc9e3f
Ashur Cabrera <hello@ashur.cab>
2025-04-18 21:09:15 -0700

    aliases: add start=serve

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,7 @@ alias la="ls -hAlp"
 alias nova.="nova ."
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='omz reload'
+alias start=serve
 
 serve() {
 	if [ -f .aliases.zsh ]; then
