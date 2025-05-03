8e0a099d079b6112059f72d7fee6deaa5f71f332
Ashur Cabrera <hello@ashur.cab>
2025-05-03 08:53:01 -0700

    aliases: rename to start, start_local

--- aliases.zsh
+++ aliases.zsh
@@ -6,17 +6,17 @@ alias la="ls -hAlp"
 alias nova.="nova ."
 alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '🔑  Public key copied to pasteboard.'"
 alias reload!='omz reload'
-alias start=serve
+alias serve=start
 
-serve() {
+start() {
 	if [ -f .aliases.zsh ]; then
-		serve_local() {
-			echo "'serve_local' not defined in $PWD/.aliases.zsh"
+		start_local() {
+			echo "'start_local' not defined in $PWD/.aliases.zsh"
 			return 1
 		}
 
 		source .aliases.zsh
-		serve_local
+		start_local
 	else
 		echo "$PWD/.aliases.zsh not found"
 		return 1
