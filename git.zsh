5f836ca2be51edb1db1fb35711679988adbdc633
Ashur Cabrera <hello@ashur.cab>
2026-07-23 22:12:30 -0700

    git: gbclean

--- git.zsh
+++ git.zsh
@@ -2,3 +2,7 @@ alias gco='git checkout'
 alias gs='git status'
 alias gw='git whatchanged'
 alias gpu='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+
+gbclean() {
+	git branch -vv | grep 'gone]' | awk '{print $1}' | xargs git branch -D
+}
